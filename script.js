06fd64fbd925e6d9b9f8f98c7e04d4061d807be0
IskandarAyuphan <iskandarayup@gmail.com>
2026-01-01 12:59:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Scroll animation for all sections except Home
 const sections = document.querySelectorAll(".section");
 
 window.addEventListener("scroll", () => {
   const trigger = window.innerHeight * 0.85;
 
   sections.forEach(section => {
+    if(section.id === "home") return; // skip home section
+
     const top = section.getBoundingClientRect().top;
     if (top < trigger) {
       section.style.opacity = 1;
